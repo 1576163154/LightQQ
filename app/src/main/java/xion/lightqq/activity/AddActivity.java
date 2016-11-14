ecf6db8a182f9081a66d32154ff4323a3ca9a30c
15761631504 <849483053@qq.com>
2016-11-14 10:43:16 +0800

    11.14 commit 新增滑动选择性别 O(∩_∩)O

--- app/src/main/java/xion/lightqq/activity/AddActivity.java
+++ app/src/main/java/xion/lightqq/activity/AddActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import xion.lightqq.R;
 
@@ -15,6 +16,7 @@ import xion.lightqq.R;
 
 public class AddActivity extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout ll_addForG;
+    private RelativeLayout rl_add_findby;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +27,11 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
 
     private void initWidget() {
         ll_addForG = (LinearLayout) findViewById(R.id.ll_addFofG);
+        rl_add_findby = (RelativeLayout) findViewById(R.id.rl_add_findby);
     }
     private void setWidget(){
         ll_addForG.setOnClickListener(this);
+        rl_add_findby.setOnClickListener(this);
     }
 
     @Override
@@ -37,6 +41,8 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
                 startActivity(new Intent(AddActivity.this,AddFriendorGroupActivity.class));
                 finish();
                 break;
+            case R.id.rl_add_findby:
+                startActivity(new Intent(AddActivity.this,FindByActivity.class));
             default:
                 break;
         }
--- app/src/main/java/xion/lightqq/activity/FindByActivity.java
+++ app/src/main/java/xion/lightqq/activity/FindByActivity.java
@@ -3,19 +3,36 @@ package xion.lightqq.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 import xion.lightqq.R;
+import xion.lightqq.widget.WheelView;
 
 /**
  * Created by Administrator on 2016/10/26.
  */
 
-public class FindByActivity extends Activity {
+public class FindByActivity extends Activity implements View.OnClickListener{
     private SearchView sv_nickname;
+    //性别年龄wheelview所需原始数据
+    private final String[] sex = {"男","女","不限"};
+    private final String[] age = {"不限","18岁以下","18-22岁","23-26岁","27-35岁","35岁以上"};
+    private RelativeLayout rl_findby_sex;
+    private TextView tv_findby_sex;
+    private RelativeLayout rl_findby_age;
+    private TextView tv_findby_age;
+    private AlertDialog sexDialog;//选择性别对话框
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +42,10 @@ public class FindByActivity extends Activity {
     }
     private void initWidget() {
         sv_nickname = (SearchView) findViewById(R.id.sv_nicknameorkeyword);
+        rl_findby_sex = (RelativeLayout) findViewById(R.id.rl_findby_sex);
+        rl_findby_age = (RelativeLayout) findViewById(R.id.rl_findby_age);
+        tv_findby_age = (TextView) findViewById(R.id.tv_findby_age);
+        tv_findby_sex = (TextView) findViewById(R.id.tv_findby_sex);
     }
 
     private void setWidget() {
@@ -38,5 +59,41 @@ public class FindByActivity extends Activity {
         }
 //        sv_nickname.onActionViewExpanded();
         sv_nickname.setIconified(false);
+
+        rl_findby_sex.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            //设置 性别，年龄相关点击dialog和wheelview事件
+            case R.id.rl_findby_sex:
+                View WVL = LayoutInflater.from(this).inflate(R.layout.wheel_view,null);
+                WheelView sexWV = (WheelView) WVL.findViewById(R.id.wv);
+                sexWV.setOffset(1);//设置初始偏移量
+                sexWV.setItems(Arrays.asList(sex));
+                sexWV.setSeletion(1);//设置初始选中索引为1的选项
+                sexWV.setOnWheelViewListener(new WheelView.OnWheelViewListener(){
+                    @Override
+                    public void onSelected(int selectedIndex, String item) {
+                        tv_findby_sex.setText(item);
+                    }
+                });
+                View dialogtitleView =LayoutInflater.from(this).inflate(R.layout.dialog_title,null);
+                TextView tv_dialog_title = (TextView) dialogtitleView.findViewById(R.id.tv_dialog_title);
+                tv_dialog_title.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sexDialog.dismiss();
+
+                    }
+                });
+                sexDialog = new AlertDialog.Builder(this)
+                        .setCustomTitle(dialogtitleView)
+                        .setView(WVL)
+                        .show();
+                break;
+            case R.id.rl_findby_age:
+        }
     }
 }
