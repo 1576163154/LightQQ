8be251a36383cfca5795346703f6a40dfa97b285
15761631504 <849483053@qq.com>
2016-11-14 11:17:36 +0800

    11.14 commit

--- app/src/main/java/xion/lightqq/activity/FindByActivity.java
+++ app/src/main/java/xion/lightqq/activity/FindByActivity.java
@@ -4,10 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
@@ -32,6 +30,9 @@ public class FindByActivity extends Activity implements View.OnClickListener{
     private RelativeLayout rl_findby_age;
     private TextView tv_findby_age;
     private AlertDialog sexDialog;//选择性别对话框
+    private AlertDialog ageDialog;//选择年龄框
+    private View WVL;//wheelview 布局
+    private View dialogtitleView;//自定义dialog标题布局
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +47,9 @@ public class FindByActivity extends Activity implements View.OnClickListener{
         rl_findby_age = (RelativeLayout) findViewById(R.id.rl_findby_age);
         tv_findby_age = (TextView) findViewById(R.id.tv_findby_age);
         tv_findby_sex = (TextView) findViewById(R.id.tv_findby_sex);
+        tv_findby_age = (TextView) findViewById(R.id.tv_findby_age);
+        WVL = LayoutInflater.from(this).inflate(R.layout.wheel_view,null);
+        dialogtitleView =LayoutInflater.from(this).inflate(R.layout.dialog_title,null);
     }
 
     private void setWidget() {
@@ -61,6 +65,7 @@ public class FindByActivity extends Activity implements View.OnClickListener{
         sv_nickname.setIconified(false);
 
         rl_findby_sex.setOnClickListener(this);
+        rl_findby_age.setOnClickListener(this);
     }
 
     @Override
@@ -68,7 +73,6 @@ public class FindByActivity extends Activity implements View.OnClickListener{
         switch (v.getId()){
             //设置 性别，年龄相关点击dialog和wheelview事件
             case R.id.rl_findby_sex:
-                View WVL = LayoutInflater.from(this).inflate(R.layout.wheel_view,null);
                 WheelView sexWV = (WheelView) WVL.findViewById(R.id.wv);
                 sexWV.setOffset(1);//设置初始偏移量
                 sexWV.setItems(Arrays.asList(sex));
@@ -79,9 +83,8 @@ public class FindByActivity extends Activity implements View.OnClickListener{
                         tv_findby_sex.setText(item);
                     }
                 });
-                View dialogtitleView =LayoutInflater.from(this).inflate(R.layout.dialog_title,null);
-                TextView tv_dialog_title = (TextView) dialogtitleView.findViewById(R.id.tv_dialog_title);
-                tv_dialog_title.setOnClickListener(new View.OnClickListener() {
+                TextView tv_sexdialog_title = (TextView) dialogtitleView.findViewById(R.id.tv_dialog_title);
+                tv_sexdialog_title.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         sexDialog.dismiss();
@@ -94,6 +97,30 @@ public class FindByActivity extends Activity implements View.OnClickListener{
                         .show();
                 break;
             case R.id.rl_findby_age:
+                WheelView ageWV = (WheelView) WVL.findViewById(R.id.wv);
+                ageWV.setOffset(1);
+                ageWV.setItems(Arrays.asList(age));
+                ageWV.setSeletion(1);
+                ageWV.setOnWheelViewListener(new WheelView.OnWheelViewListener(){
+                    @Override
+                    public void onSelected(int selectedIndex, String item) {
+                        tv_findby_age.setText(item);
+                    }
+                });
+                TextView tv_agedialog_title = (TextView) dialogtitleView.findViewById(R.id.tv_dialog_title);
+                tv_agedialog_title.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ageDialog.dismiss();
+                    }
+                });
+                ageDialog = new AlertDialog.Builder(this)
+                        .setCustomTitle(dialogtitleView)
+                        .setView(WVL)
+                        .show();
+                break;
+            default:
+                break;
         }
     }
 }
